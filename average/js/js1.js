d77b1840f60a63bcb4b1e06b48d1d21fc6ff035d
venkatakrishnan77088 <vkproplayer1234@gmail.com>
2022-12-01 12:14:48 +0530

    adding program

---
+++ average/js/js1.js
@@ -0,0 +1,10 @@
+
+var name=prompt("enter the name ");
+var a=parseInt(prompt("enter the tamil mark"));
+var b=parseInt(prompt("enter the english mark"));
+var c=parseInt(prompt("enter the maths mark"));
+var d=parseInt(prompt("enter the science mark"));
+var e=parseInt(prompt("enter the social mark"));
+x=a+b+c+d+e;
+average=x/5;
+alert(average);
---
+++ sum/js/js1.js
@@ -0,0 +1,4 @@
+const num1=parseInt(prompt("enter a number"));
+const num2=parseInt(prompt("enter a number"));
+const sum=num1+num2;
+alert(sum);
